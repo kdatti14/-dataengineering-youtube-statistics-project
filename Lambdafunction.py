db9cbfeb8b319a45d17df49119b9b8a15cb7a5da
Katyayani Datti <163447820+kdatti14@users.noreply.github.com>
2024-11-05 15:37:45 -0700

    Create Lambdafunction.py

---
+++ Lambdafunction.py
@@ -0,0 +1,39 @@
+# Import Libraries 
+import awswrangler as wr
+import pandas as pd
+import urllib.parse
+import os
+
+# Environment Variable Setup
+os_input_s3_cleansed_layer = os.environ['s3_cleansed_layer']
+os_input_glue_catalog_db_name = os.environ['glue_catalog_db_name']
+os_input_glue_catalog_table_name = os.environ['glue_catalog_table_name']
+os_input_write_data_operation = os.environ['write_data_operation']
+
+# Lambda Handler Function
+def lambda_handler(event, context):
+
+  # Extract S3 Bucket and Object Key
+  # From the S3 event that triggered the function, it extracts the bucket name and the key (file path) of the uploaded JSON file.
+  # urllib.parse.unquote_plus decodes any URL-encoded characters.
+  bucket = event['Records'][0]['s3']['bucket']['name']
+  key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+  try:
+    # Read JSON Data into DataFrame & Normalize JSON Structure
+    df_raw = wr.s3.read_json('s3://{}/{}'.format(bucket, key))
+    df_step_1 = pd.json_normalize(df_raw['items'])
+    
+    # Write Data to S3 as Parquet
+    wr_response = wr.s3.to_parquet(
+        df=df_step_1,
+        path=os_input_s3_cleansed_layer,
+        dataset=True,
+        database=os_input_glue_catalog_db_name,
+        table=os_input_glue_catalog_table_name,
+        mode=os_input_write_data_operation
+    )
+    return wr_response
+  except Exception as e:
+    print(e)
+    print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+    raise e
